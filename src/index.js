ec0976956b160daa005382980b8d223c6875be01
KoTuKJuBoTuK <simonovantond@gmail.com>
2018-03-09 18:53:32 +0300

    eval is power

--- src/index.js
+++ src/index.js
@@ -1,27 +1,38 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.initialValue = initialValue.toString();
   }
 
   add(number) {
-    // your implementation
+    this.initialValue += " + " + number;
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.initialValue += " - " + number;
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.initialValue += " * " + number;
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.initialValue += " / " + number;
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.initialValue += " ** " + number;
+    return this;
+  }
+
+  valueOf(){
+    return eval(this.initialValue);
   }
 }
+
+
 
 module.exports = SmartCalculator;
